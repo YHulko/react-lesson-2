0c8396d3c8ddc9c12453c183de2a0c42001944df
Yuka <opfr95@gmail.com>
2023-07-28 19:59:45 +0300

    not

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -11,7 +11,38 @@ type Props = {
     image: string
 }
 
-class ProductListItem extends Component<Props> {
+type State = {
+    count: number
+    color: string
+}
+
+class ProductListItem extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props)
+        this.state = {
+            count: 1,
+            color: 'green',
+        }
+        this.onIncrementClick = this.onIncrementClick.bind(this)
+    }
+
+    onIncrementClick() {
+        this.setState((prevState) => ({
+            count: prevState.count + 1,
+        }))
+    }
+
+    onDecrementClick() {
+        this.setState((prevState) => ({
+            count: prevState.count - 1,
+        }))
+    }
+    changeColor = () => {
+        this.setState((prevState) => ({
+            color: prevState.color === 'green' ? 'red' : 'green',
+        }))
+    }
+
     render() {
         const { title, description, type, capacity, price, image } = this.props
 
@@ -30,10 +61,28 @@ class ProductListItem extends Component<Props> {
                     <div className="product-price">
                         Price: <span>${price}</span>
                     </div>
+
+                    <div>
+                        Color: <span></span>
+                        {this.state.color}
+                    </div>
+                    <button>Change color</button>
+
                     <div className="product-quantity">
-                        <Button variant="outlined">-</Button>
-                        <TextField value="1" size="small" />
-                        <Button variant="outlined">+</Button>
+                        <Button
+                            variant="outlined"
+                            onClick={() => this.onDecrementClick()}
+                            disabled={this.state.count <= 1}
+                        >
+                            -
+                        </Button>
+                        <TextField value={this.state.count} size="small" />
+                        <Button
+                            variant="outlined"
+                            onClick={this.onIncrementClick}
+                        >
+                            +
+                        </Button>
                     </div>
                     <div className="btns-wrap">
                         <Button variant="outlined">Add to cart</Button>
