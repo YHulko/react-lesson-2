c897ce245a241660e2e7889a534e040c7ac4538d
YHulko <opfr95@gmail.com>
2023-07-31 14:53:05 +0300

    useState

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -2,9 +2,21 @@ import Header from "../Header/Header"
 import CssBaseline from '@mui/material/CssBaseline';
 import { StyledEngineProvider } from '@mui/material/styles'
 import Main from 'container/Main/Main'
+import { useState } from 'react'
 
 type Props = {}
+
+type CardDataProps ={
+  totalCount: number,
+  totalPrice: number
+}
+
+
 const App = (props: Props) => {
+  const [cardData, setCardData] = useState<CardDataProps>({
+    totalCount: 10,
+    totalPrice: 100,
+  })
   return (
     <StyledEngineProvider injectFirst>
      <CssBaseline />
